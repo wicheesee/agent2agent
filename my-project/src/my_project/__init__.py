8ffd72dc58454aa6e149bc1a0989ed52028f06fd
wicheesee <144334546+wicheesee@users.noreply.github.com>
2025-04-28 16:08:56 +0700

    agent2agent

---
+++ my-project/src/my_project/__init__.py
@@ -0,0 +1,51 @@
+import logging
+import asyncio
+import click
+from dotenv import load_dotenv
+import google_a2a
+from google_a2a.common.types import AgentSkill, AgentCapabilities, AgentCard
+from google_a2a.common.server import A2AServer
+from my_project.task_manager import MyAgentTaskManager
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@click.command()
+@click.option("--host", default="localhost")
+@click.option("--port", default=10002)
+def main(host, port):
+  skill = AgentSkill(
+    id="my-project-echo-skill",
+    name="Echo Tool",
+    description="Echos the input given",
+    tags=["echo", "repeater"],
+    examples=["I will see this echoed back to me"],
+    inputModes=["text"],
+    outputModes=["text"],
+  )
+  logging.info(skill)
+  capabilities = AgentCapabilities(
+    streaming=True
+  )
+  agent_card = AgentCard(
+    name="Echo Agent",
+    description="This agent echos the input given",
+    url=f"http://{host}:{port}/",
+    version="0.1.0",
+    defaultInputModes=["text"],
+    defaultOutputModes=["text"],
+    capabilities=capabilities,
+    skills=[skill]
+  )
+  logging.info(agent_card)
+  task_manager = MyAgentTaskManager()
+  server = A2AServer(
+    agent_card=agent_card,
+    task_manager=task_manager,
+    host=host,
+    port=port,
+  )
+  server.start()
+
+if __name__ == "__main__":
+  main()
---
+++ my-project/src/my_project/task_manager.py
@@ -0,0 +1,115 @@
+from typing import AsyncIterable
+import asyncio
+import google_a2a
+from google_a2a.common.server.task_manager import InMemoryTaskManager
+from google_a2a.common.types import (
+  Artifact,
+  JSONRPCResponse,
+  Message,
+  SendTaskRequest,
+  SendTaskResponse,
+  SendTaskStreamingRequest,
+  SendTaskStreamingResponse,
+  Task,
+  TaskState,
+  TaskStatus,
+  TaskStatusUpdateEvent,
+)
+
+class MyAgentTaskManager(InMemoryTaskManager):
+  def __init__(self):
+    super().__init__()
+
+  async def on_send_task(self, request: SendTaskRequest) -> SendTaskResponse:
+    # Upsert a task stored by InMemoryTaskManager
+    await self.upsert_task(request.params)
+
+    task_id = request.params.id
+    # Our custom logic that simply marks the task as complete
+    # and returns the echo text
+    received_text = request.params.message.parts[0].text
+    task = await self._update_task(
+      task_id=task_id,
+      task_state=TaskState.COMPLETED,
+      response_text=f"on_send_task received: {received_text}"
+    )
+
+    # Send the response
+    return SendTaskResponse(id=request.id, result=task)
+
+  async def _stream_3_messages(self, request: SendTaskStreamingRequest):
+    task_id = request.params.id
+    received_text = request.params.message.parts[0].text
+
+    text_messages = ["one", "two", "three"]
+    for text in text_messages:
+      parts = [
+        {
+          "type": "text",
+          "text": f"{received_text}: {text}",
+        }
+      ]
+      message = Message(role="agent", parts=parts)
+      is_last = text == text_messages[-1]
+      task_state = TaskState.COMPLETED if is_last else TaskState.WORKING
+      task_status = TaskStatus(
+        state=task_state,
+        message=message
+      )
+      task_update_event = TaskStatusUpdateEvent(
+        id=request.params.id,
+        status=task_status,
+        final=is_last,
+      )
+      await self.enqueue_events_for_sse(
+        request.params.id,
+        task_update_event
+      )
+
+  async def on_send_task_subscribe(
+    self,
+    request: SendTaskStreamingRequest
+  ) -> AsyncIterable[SendTaskStreamingResponse] | JSONRPCResponse:
+    # Upsert a task stored by InMemoryTaskManager
+    await self.upsert_task(request.params)
+
+    task_id = request.params.id
+    # Create a queue of work to be done for this task
+    sse_event_queue = await self.setup_sse_consumer(task_id=task_id)
+
+    # Start the asynchronous work for this task
+    asyncio.create_task(self._stream_3_messages(request))
+
+    # Tell the client to expect future streaming responses
+    return self.dequeue_events_for_sse(
+      request_id=request.id,
+      task_id=task_id,
+      sse_event_queue=sse_event_queue,
+    )
+
+  async def _update_task(
+    self,
+    task_id: str,
+    task_state: TaskState,
+    response_text: str,
+  ) -> Task:
+    task = self.tasks[task_id]
+    agent_response_parts = [
+      {
+        "type": "text",
+        "text": response_text,
+      }
+    ]
+    task.status = TaskStatus(
+      state=task_state,
+      message=Message(
+        role="agent",
+        parts=agent_response_parts,
+      )
+    )
+    task.artifacts = [
+      Artifact(
+        parts=agent_response_parts,
+      )
+    ]
+    return task
